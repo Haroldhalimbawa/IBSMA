76b93bf5ec302e1380b23dae32c3025f4f4bf4a6
Haroldhalimbawa <128500848+Haroldhalimbawa@users.noreply.github.com>
2023-03-24 21:58:35 +0800

    Add files via upload

--- form-handler.php
+++ form-handler.php
@@ -4,7 +4,7 @@ $visitor_email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 
-$email_from = 'info@ibsma1.000webhostapp.com';
+$email_from = 'ibsmaofficial.netlify.app';
 
 $email_subject = 'New Form Sumission';
 
